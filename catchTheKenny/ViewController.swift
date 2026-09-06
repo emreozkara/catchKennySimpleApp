8a826dfed207adfb5a949aa63f128f647e5559a6
Emre Ozkara <emreozkara1010@gmail.com>
2026-09-06 14:28:05 +0300

    Initial Commit

---
+++ catchTheKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  catchTheKenny
+//
+//  Created by Emre Özkara on 6.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ catchTheKennyTests/catchTheKennyTests.swift
@@ -0,0 +1,19 @@
+//
+//  catchTheKennyTests.swift
+//  catchTheKennyTests
+//
+//  Created by Emre Özkara on 6.09.2026.
+//
+
+import Testing
+@testable import catchTheKenny
+
+struct catchTheKennyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
